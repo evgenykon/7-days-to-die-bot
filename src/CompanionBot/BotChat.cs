f1a8e594780652ce5b6b9dce15451f55c1d21fea
effus <effusps@gmail.com>
2026-06-04 01:38:55 +0300

    botsay: try all chat targets (ChatOutput, Chat, SendMessageDelegate)

--- src/CompanionBot/BotChat.cs
+++ src/CompanionBot/BotChat.cs
@@ -32,24 +32,68 @@ public class ConsoleCmdBotSay : ConsoleCmdAbstract
             if (localPlayer != null)
             {
                 var xui = localPlayer.PlayerUI.xui;
+                var hit = false;
 
-                XUiC_ChatOutput chatOutput = null;
                 foreach (var wg in xui.WindowGroups)
                 {
-                    if (wg.ID == "chatoutput")
+                    if (wg.ID == "chatoutput" || wg.ID == "chat")
                     {
-                        chatOutput = wg.Controller as XUiC_ChatOutput;
-                        break;
+                        var co = wg.Controller as XUiC_ChatOutput;
+                        if (co != null)
+                        {
+                            Log.Out($"[CB] Try XUiC_ChatOutput on '{wg.ID}'");
+                            co.addMessage(EnumGameMessages.Chat, EChatType.Global, EChatDirection.None, "Quinn", chatMsg, "");
+                            hit = true;
+                        }
+
+                        var ch = wg.Controller as XUiC_Chat;
+                        if (ch != null)
+                        {
+                            Log.Out($"[CB] Try XUiC_Chat on '{wg.ID}'");
+                            ch.TextInput_OnSubmitHandler(ch, chatMsg);
+                            hit = true;
+                        }
+
+                        var children = wg.Controller.children;
+                        if (children != null)
+                        {
+                            foreach (var child in children)
+                            {
+                                var childCo = child as XUiC_ChatOutput;
+                                if (childCo != null)
+                                {
+                                    Log.Out($"[CB] Try child XUiC_ChatOutput on '{wg.ID}'");
+                                    childCo.addMessage(EnumGameMessages.Chat, EChatType.Global, EChatDirection.None, "Quinn", chatMsg, "");
+                                    hit = true;
+                                }
+                                var childCh = child as XUiC_Chat;
+                                if (childCh != null)
+                                {
+                                    Log.Out($"[CB] Try child XUiC_Chat on '{wg.ID}'");
+                                    childCh.TextInput_OnSubmitHandler(childCh, chatMsg);
+                                    hit = true;
+                                }
+                            }
+                        }
                     }
                 }
 
-                if (chatOutput != null)
+                if (XUiC_Chat.messagingHandlers != null)
                 {
-                    chatOutput.addMessage(EnumGameMessages.Chat, EChatType.Global, EChatDirection.None, "Quinn", chatMsg, "");
-                    return;
+                    foreach (var handler in XUiC_Chat.messagingHandlers)
+                    {
+                        if (handler != null && handler.SendMessageDelegate != null)
+                        {
+                            Log.Out($"[CB] Try SendMessageDelegate");
+                            handler.SendMessageDelegate(EChatType.Global, localPlayer.entityId.ToString(), chatMsg);
+                            hit = true;
+                            break;
+                        }
+                    }
                 }
 
-                Log.Out("[CB] XUiC_ChatOutput not found by ID='chatoutput'");
+                if (!hit)
+                    Log.Out("[CB] No chat output found");
             }
 
             SdtdConsole.Instance.Output(chatMsg);
